7261460e34473d67c57680f9e796e424a611663c
Cristian Torrano <cristian.torrano@xoor.io>
2025-10-01 12:52:02 -0300

    clean repo

--- app/routes/auth.callback/route.tsx
+++
@@ -1,8 +0,0 @@
-import { authenticate } from "../../shopify.server";
-
-export const loader = ({ request }: { request: Request }) => {
-  console.log(`[CALLBACK] Route hit: ${request.url}`);
-  const result = authenticate.admin(request);
-  console.log(`[CALLBACK] Processing OAuth callback`);
-  return result;
-};
